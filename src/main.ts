c9bb807238915c9211a2ffbc3ab504429f4a0b33
Pierre steve <stevepierre17@gmail.com>
2025-08-07 19:47:46 -0400

    Update on main file

--- src/main.ts
+++ src/main.ts
@@ -21,10 +21,21 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0', () => {
-    Logger.verbose(`Application is running on PORT: ${process.env.PORT}`);
-    Logger.verbose(`Application is running on: http://localhost:${process.env.PORT}`);
-    Logger.verbose(`API Documentation is running on: http://localhost:${process.env.PORT}/api/docs`);
-  });
+  // await app.listen(process.env.PORT ?? 3000, '0.0.0.0', () => {
+  //   Logger.verbose(`Application is running on PORT: ${process.env.PORT}`);
+  //   Logger.verbose(`Application is running on: http://localhost:${process.env.PORT}`);
+  //   Logger.verbose(`API Documentation is running on: http://localhost:${process.env.PORT}/api/docs`);
+  // });
+  
+   // Convertir PORT en number et fallback
+  const port = parseInt(process.env.PORT ?? '3000', 10);
+
+  // Bind explicitement à 0.0.0.0 (exigé par Render)
+  await app.listen(port, '0.0.0.0');
+
+  // Logging clair
+  logger.log(`Application démarrée — écoute sur 0.0.0.0:${port}`);
+  logger.log(`Préfixe global: /api (routes versionnées via URI)`);
+  logger.log(`Swagger UI: /api/docs`);
 }
 bootstrap();
